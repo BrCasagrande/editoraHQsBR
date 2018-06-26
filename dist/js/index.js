3bad8e61c802c6e663c6027cc245ca393c6b7c26
BrCasagrande <brunopcasagrande@gmail.com>
2018-06-26 17:38:21 -0300

    Produto final

---
+++ dist/js/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var pr = 'banana';
+var num = ['veqweqwc2222', 'chocolate', 'bala'];
+
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+    for (var _iterator = num[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var n = _step.value;
+
+        console.log(n);
+    }
+} catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+} finally {
+    try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+        }
+    } finally {
+        if (_didIteratorError) {
+            throw _iteratorError;
+        }
+    }
+}
